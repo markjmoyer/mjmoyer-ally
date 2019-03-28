c67e6e7ba9630de8bd0dc8a587cbdb0d734315b2
Mark J. Moyer <mjm@markjmoyer.com>
2019-03-28 19:32:21 -0400

    gulpfile task edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,57 @@
-let gulp = require('gulp');
-
+var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var uglifycss = require('uglifycss');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function() {
- // place code for your default task here
+ //place code for default task here
 });
-
-// Lint Task
+// run lint task
 gulp.task('lint', function() {
-    return gulp.src('src/assets/js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+  return gulp.src('src/assets/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
-// Compile Our Sass
+// run sass task
 gulp.task('sass', function() {
-    return gulp.src('src/assets/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/assets/css'));
+  return gulp.src('src/assets/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/assets/css'));
 });
-// Concatenate & Minify JS
+// run scripts task and concatenate
 gulp.task('scripts', function() {
-    return gulp.src('src/assets/js/*.js')
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('dist/assets/js'))
-        .pipe(rename('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/assets/js'));
+  return gulp.src('src/assets/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'));
 });
-//copy fonts into dist directory
+// run copy-fonts task
 gulp.task('copy-fonts', function() {
-   gulp.src('src/assets/fonts/**/*.{ttf,woff,eof,svg}')
-   .pipe(gulp.dest('./dist'));
+  return gulp.src('src/assets/fonts/**/*.{ttf,woff,woff2,eot,otf,svg}')
+   .pipe(gulp.dest('./dist/assets/fonts'));
 });
-//copy index.html into dist
+// run copy-files task
 gulp.task('copy-files', function() {
-    gulp.src('src/index.html')
-    // Perform minification tasks, etc here
+  return gulp.src('src/index.html')
     .pipe(gulp.dest('./dist/'));
 });
-// Watch Files For Changes
+// run minify css task
+gulp.task('minify-css', function() {
+  return gulp.src('src/assets/css/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/assets/css'));
+});
+// run watch task - listen for changes
 gulp.task('watch', function() {
-    gulp.watch('src/assets/js/*.js', ['lint', 'scripts']);
-    gulp.watch('src/assets/scss/*.scss', ['sass']);
-    gulp.watch('src/*.html', ['copy-files']);
+  gulp.watch('src/assets/js/*.js', ['lint', 'scripts']);
+  gulp.watch('src/assets/scss/*.scss', ['sass']);
+  gulp.watch('src/*.html', ['copy-files']);
 });
 
-// Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'copy-fonts', 'copy-files', 'watch']);
+gulp.task('default', ['lint', 'sass', 'scripts', 'copy-fonts', 'copy-files', 'minify-css', 'watch']);
